e4ea9e66a5af11be95e77406d47f2ee4d6b8542d
Чадаев Николай <n.chadaev.tech>
2022-07-20 19:24:32 +0300

    Stage_1.csv изменён.

---
+++ fias_loader/DBMS/CreateExtensions.sql
@@ -0,0 +1,7 @@
+--
+-- 2021-12-02 Nick Создание необходимых расширений.
+--
+CREATE EXTENSION plpgsql_check SCHEMA public; 
+CREATE EXTENSION hstore SCHEMA public;        
+CREATE EXTENSION postgres_fdw SCHEMA public;  
+CREATE EXTENSION dblink SCHEMA gar_link;

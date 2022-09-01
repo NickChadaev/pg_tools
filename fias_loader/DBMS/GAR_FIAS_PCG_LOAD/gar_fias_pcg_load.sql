a4401cf497ed51cdc4645b5afc64424461bf2638
Чадаев Николай <n.chadaev.tech>
2022-09-01 17:09:17 +0300

    The "gar_fias_pcg_load" was updatted.

--- fias_loader/DBMS/GAR_FIAS_PCG_LOAD/gar_fias_pcg_load.sql
+++ fias_loader/DBMS/GAR_FIAS_PCG_LOAD/gar_fias_pcg_load.sql
@@ -828,6 +828,125 @@ COMMENT ON FUNCTION gar_fias_pcg_load.f_addr_obj_get_twins (varchar(250))
 --   EXPLAIN   SELECT * FROM gar_fias_pcg_load.f_addr_obj_get_twins ('вОЛКОВО');
 
 -- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+DROP FUNCTION IF EXISTS gar_fias_pcg_load.f_addr_obj_update_children (uuid, uuid, date);
+
+DROP FUNCTION IF EXISTS gar_fias_pcg_load.f_addr_obj_update_children (date, integer[], uuid[], date);
+CREATE OR REPLACE FUNCTION gar_fias_pcg_load.f_addr_obj_update_children (
+         p_date_1     date
+        ,p_obj_level  integer[] = ARRAY[6,7] 
+        ,p_fias_guid  uuid[]    = NULL::uuid[]
+        ,p_date_2     date      = current_date
+         --
+        ,OUT fias_guid_new    uuid
+        ,OUT nm_addr_obj      varchar(250) 
+        ,OUT addr_obj_type_id bigint 
+        ,OUT chld_qty_tot     integer
+        ,OUT chld_qty_unact   integer
+  )
+    RETURNS setof record
+    LANGUAGE plpgsql 
+    SECURITY DEFINER
+    AS $$
+    -- ================================================================================
+    -- Author: Nick
+    -- Create date: 2022-08-31/2022-09-01
+    -- -------------------------------------------------------------------------------- 
+    -- Модификация отношения подчинённости в схеме gar_fias.
+    --      Для выбранного родителя, оставляю актуальных детей.
+    -- ================================================================================
+  DECLARE
+    _rec_one record; 
+    
+  BEGIN
+    -- ------------------------------------------------------------------------------
+    -- Цикл по таблице ошибок. (obj_level IN (6,7)). Парные записи, вначале новый 
+    --   адресный объект, потом старый. 
+    -- Должны совпадать: id_addr_parent, upper(nm_addr_obj), addr_obj_type_id.
+    -- Далее обработка (пункты 0,1,2). 
+    -- Чтение одной записи (старая/новая  пока вопрос)
+    -- Тройка атрибутов совпала -- это ещё одна старая -- обработка (пункты 0,1,2).
+    -- Тройка не совпала, это новая запись !!! Ура - новая, читаю следующую - старая
+    -- и Обработка по пунктам 0,1,2
+    -- -----------------------------------------------------------------------------
+  
+    FOR _rec_one IN 
+     WITH z (  change_id
+              ,id_addr_parent
+              ,nm_addr_obj
+              ,addr_obj_type_id
+         ) AS 
+           (
+             SELECT  MAX(x.change_id) AS change_id
+                    ,x.id_addr_parent
+                    ,upper(x.nm_addr_obj)
+                    ,x.addr_obj_type_id
+                   
+                  FROM gar_fias.gap_adr_area x 
+                    WHERE (x.obj_level = ANY(p_obj_level)) AND 
+                          (x.date_create = p_date_1) AND
+                          (((x.fias_guid = ANY (p_fias_guid)) AND (p_fias_guid IS NOT NULL))
+                             OR (p_fias_guid IS NULL)
+                          )	                                  
+                   GROUP BY x.id_addr_parent, upper(x.nm_addr_obj), x.addr_obj_type_id  
+            )
+              SELECT a.fias_guid AS fias_guid_new
+                   , b.fias_guid AS fias_guid_old 
+                   , a.id_addr_parent  
+                   , a.nm_addr_obj 
+                   , a.addr_obj_type_id              
+                   , a.change_id AS change_id_new
+                   , b.change_id AS change_id_old
+              FROM z
+                JOIN gar_fias.gap_adr_area a ON (z.id_addr_parent = a.id_addr_parent)  AND
+                                                (z.nm_addr_obj = upper(a.nm_addr_obj)) AND
+                                                (z.addr_obj_type_id = a.addr_obj_type_id) AND
+                                                (z.change_id = a.change_id) 
+                --                                
+                JOIN gar_fias.gap_adr_area b ON (z.id_addr_parent = b.id_addr_parent)  AND
+                                                (z.nm_addr_obj = upper(b.nm_addr_obj)) AND
+                                                (z.addr_obj_type_id = b.addr_obj_type_id) AND
+                                                (z.change_id > b.change_id)		  
+
+      LOOP
+          -- 0) Заменяю старого родителя на нового
+          chld_qty_tot := gar_fias_pcg_load.f_addr_obj_update_parent (
+                            _rec_one.fias_guid_old, _rec_one.fias_guid_new, p_date_2
+          );
+          
+          -- 1) Старого родителя убрать: is actual, is_active <---- FALSE.
+          UPDATE gar_fias.as_addr_obj SET is_actual = FALSE, is_active = FALSE
+           WHERE (object_guid = _rec_one.fias_guid_old);
+         
+          -- 2) Убрать неактуальных детей. (id_lead IS not NULL) убрать  is actual, is_active <---- FALSE.
+          WITH z (id_addr_obj)
+              AS (
+                   SELECT id_addr_obj 
+                       FROM gar_fias_pcg_load.f_adr_area_show_data (_rec_one.fias_guid_new)
+                              WHERE (id_lead IS not NULL)
+                 )
+             UPDATE gar_fias.as_addr_obj x SET is_actual = FALSE, is_active = FALSE
+              FROM z
+                      WHERE (x.object_id = z.id_addr_obj);
+        
+        GET DIAGNOSTICS chld_qty_unact = ROW_COUNT;
+        
+        fias_guid_new    := _rec_one.fias_guid_new;
+        nm_addr_obj      := _rec_one.nm_addr_obj; 
+        addr_obj_type_id := _rec_one.addr_obj_type_id;
+        
+        RETURN NEXT;
+      END LOOP;
+  END;
+ $$;
+
+COMMENT ON FUNCTION gar_fias_pcg_load.f_addr_obj_update_children (date, integer[], uuid[], date) IS 
+'ТОЛЬКО АКТИВНЫЕ И АКТУАЛЬНЫЕ ОБЪЕКТЫ. Модификация отношения подчинённости в схеме gar_fias,
+с дективацией более ранних объектов.';
+--
+--  USE CASE:
+--
+
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 DROP PROCEDURE IF EXISTS gar_fias_pcg_load.p_alt_tbl (boolean);
 CREATE OR REPLACE PROCEDURE gar_fias_pcg_load.p_alt_tbl (
         p_all  boolean = TRUE

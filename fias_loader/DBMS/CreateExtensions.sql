e6ee021fc558fff22632b6cdce3942aa8b4c16bf
Чадаев Николай <n.chadaev.tech>
2022-11-25 20:34:32 +0300

    First total path

--- fias_loader/DBMS/CreateExtensions.sql
+++ fias_loader/DBMS/CreateExtensions.sql
@@ -5,3 +5,4 @@ CREATE EXTENSION plpgsql_check SCHEMA public;
 CREATE EXTENSION hstore SCHEMA public;        
 CREATE EXTENSION postgres_fdw SCHEMA public;  
 CREATE EXTENSION dblink SCHEMA gar_link;
+CREATE EXTENSION pg_trgm SCHEMA pg_catalog;
--- fias_loader/DBMS/GAR_FIAS/gar_fias_ins_tbl_as_house_type_black_list.sql
+++ fias_loader/DBMS/GAR_FIAS/gar_fias_ins_tbl_as_house_type_black_list.sql
@@ -1,28 +1,4 @@
---
--- PostgreSQL database dump
---
 
--- Dumped from database version 13.9 (Ubuntu 13.9-1.pgdg18.04+1)
--- Dumped by pg_dump version 13.9 (Ubuntu 13.9-1.pgdg18.04+1)
-
--- Started on 2022-11-21 12:02:45 MSK
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- TOC entry 3652 (class 0 OID 116159)
--- Dependencies: 350
--- Data for Name: as_house_type_black_list; Type: TABLE DATA; Schema: gar_fias; Owner: postgres
---
 
 INSERT INTO gar_fias.as_house_type_black_list (house_type_id, type_name, type_shortname, type_descr, update_date, start_date, end_date, is_active, fias_row_key) VALUES (6, 'Шахта', 'шахта', 'Шахта', '1900-01-01', '1900-01-01', '2079-06-06', true, 'шахта');
 INSERT INTO gar_fias.as_house_type_black_list (house_type_id, type_name, type_shortname, type_descr, update_date, start_date, end_date, is_active, fias_row_key) VALUES (11, 'Подвал', 'подв.', 'Подвал', '1900-01-01', '1900-01-01', '2015-11-05', false, 'подвал');
@@ -31,9 +7,4 @@ INSERT INTO gar_fias.as_house_type_black_list (house_type_id, type_name, type_sh
 INSERT INTO gar_fias.as_house_type_black_list (house_type_id, type_name, type_shortname, type_descr, update_date, start_date, end_date, is_active, fias_row_key) VALUES (14, 'Объект незавершенного строительства', 'ОНС', 'Объект незавершенного строительства', '1900-01-01', '1900-01-01', '2015-11-05', false, 'объектнезавершенногостроительства');
 
 
--- Completed on 2022-11-21 12:02:47 MSK
-
---
--- PostgreSQL database dump complete
---
 

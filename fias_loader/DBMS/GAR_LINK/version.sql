f7730e3ad3422765520060caaa8f775d2f4a044b
Николай Чадаев <n.chadaev@gd-l-gar-pp-1.abrr.local>
2022-12-29 18:40:46 +0300

    Bug fixing

--- fias_loader/DBMS/GAR_LINK/version.sql
+++ fias_loader/DBMS/GAR_LINK/version.sql
@@ -3,6 +3,6 @@
 --
 CREATE OR REPLACE VIEW gar_link.version
  AS
- SELECT '$Revision:54d152c$ modified $RevDate:2022-12-23$'::text AS version; 
+ SELECT '$Revision:c6ec2fd$ modified $RevDate:2022-12-29$'::text AS version; 
 
 -- SELECT * FROM gar_link.version;

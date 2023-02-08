26be525ed6126a163c70b7e0699c40d4cc1adf0d
Чадаев Николай <n.chadaev.tech>
2023-02-08 19:04:42 +0300

    Modification of "load_main/load_mainGar_4.py" for multy processing.

--- load_main/load_mainGar_4.py
+++ load_main/load_mainGar_4.py
@@ -20,7 +20,7 @@ from yaml.loader import SafeLoader
 
 import load_mainGar as LoadGar
 
-VERSION_STR = "  Version 0.5.6 Build 2023-02-07"
+VERSION_STR = "  Version 0.7.0 Build 2023-02-07"
 
 YAML_NOT_OPENED_0 = "... Yaml file not opened: '"
 YAML_NOT_OPENED_1 = "'."
@@ -42,91 +42,107 @@ EMP     = ""
 
 PATH_DELIMITER = '/'  # Nick 2020-05-11
 
+class make_load_4 ():
+ """
+    main class Nick 2023-02-07
+ """
+ def __init__ ( self, p_host_yaml, p_version_date ):
 
+     yaml_file_name = string.strip (p_host_yaml)
+     try:
+         fy = open (yaml_file_name, "r" )
+     
+     except IOError, ex:
+         print YAML_NOT_OPENED_0 + yaml_file_name + YAML_NOT_OPENED_1
+         sys.exit( 1 )
+     
+     self.yaml_data = yaml.load(fy, Loader=SafeLoader) 
+     self.version_date = p_version_date
+     
+ def to_do_4 (self, p_sw = False):
+     
+     if self.yaml_data.has_key ('global_params'):
+         
+         self.fserver_nmb     = self.yaml_data ['global_params'] ['g_fserver_nmb']
+         self.adr_area_sch    = self.yaml_data ['global_params'] ['g_adr_area_sch']
+         self.adr_street_sch  = self.yaml_data ['global_params'] ['g_adr_street_sch']
+         self.adr_house_sch   = self.yaml_data ['global_params'] ['g_adr_house_sch']
+         self.adr_house_sch_l = self.yaml_data ['global_params'] ['g_adr_house_sch_l']
+         self.schemas = (adr_area_sch, adr_street_sch, adr_house_sch, adr_house_sch_l)
+         
+     else:
+         self.fserver_nmb = None
+         self.schemas = None
+         
+     rc = 0
+     
+     for host in self.yaml_data ['hosts']:
+         self.h_name     = host ['name']
+         self.h_descr    = host ['descr']
+         self.h_conninfo = host ['conninfo'] 
+         h_params        = host ['params'] 
+         #
+         self.id_region       = h_params ['id_region']
+         self.batch_file_name = h_params ['exec']
+         self.path            = h_params ['path']
+         # 
+         self.batch_file_name = self.path + PATH_DELIMITER + self.batch_file_name
+         
+         cinfos = string.split (self.h_conninfo, SPACE_0)
+         for cinfo in cinfos:
+             
+             z = string.split (cinfo, S_DELIM)
+             if z[0] == 'host':
+                 self.host_ip = z[1]
+             elif z[0] == 'port':  
+                 self.port = z[1]
+             elif z[0] == 'dbname':
+                 self.db_name = z[1]
+             elif z[0] == 'user':
+                 self.user_name = z[1]
+             else:
+                 self.password = z[1]
+
+         first_str = POINTS + self.h_descr + ", id_region: " + str(self.id_region)\
+             + ", Execute: " + self.batch_file_name + ', process_id:' + str (os.getpid ())
+         if not p_sw:
+             print first_str 
+                
+         ml = LoadGar.make_load (self.host_ip, self.port, self.db_name, self.user_name)
+         rc = ml.to_do (self.host_ip, self.port, self.db_name, self.user_name, self.batch_file_name,\
+             bOUT_NAME, bERR_NAME, self.path, self.version_date, self.fserver_nmb, self.schemas,\
+                 self.id_region)     
+         
+         if not (rc == 0):
+             sys.exit (rc)    
+
+         ## if p_sw:
+         ##     import pgqueue
+         ##     put_queue
+         
+     return rc
+ 
 # ---------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     try:
         """
              Main entrypoint for the class
         """
-#                  1       2       3          4            5             6        7             
-#       sa = " <Host_IP> <Port> <DB_name> <User_name> <Batch_file_name> <Path> <Version>"
-#       sa = " <Yaml_file> <Batch_file> <Path> <Version>"
-#                  1          2           3       4   
+#                  1          2           3           
         sa = " <Yaml_file> <Version>"
 #                  1          2               
         
-        if ( len( sys.argv ) - 1 ) < 2:
+        if ((len( sys.argv ) - 1) < 2):
             print VERSION_STR 
             print "  Usage: " + str ( sys.argv [0] ) + sa
             sys.exit( 1 )
-        #
-        # Nick 2022-03-09 --------------------------------------------------
-        #
-        yaml_file_name = sys.argv[1]
-        try:
-            fy = open (yaml_file_name, "r" )
         
-        except IOError, ex:
-            print YAML_NOT_OPENED_0 + yaml_file_name + YAML_NOT_OPENED_1
-            sys.exit( 1 )
+        ml = make_load_4 (sys.argv[1], sys.argv[2])
+        if ((len (sys.argv) - 1) == 2):
+            rc = ml.to_do_4 ()
+        else:    
+            rc = ml.to_do_4 (bool(sys.argv[3]))
         
-        yaml_data = yaml.load(fy, Loader=SafeLoader) 
-        
-        if yaml_data.has_key ('global_params'):
-            
-            fserver_nmb     = yaml_data ['global_params'] ['g_fserver_nmb']
-            adr_area_sch    = yaml_data ['global_params'] ['g_adr_area_sch']
-            adr_street_sch  = yaml_data ['global_params'] ['g_adr_street_sch']
-            adr_house_sch   = yaml_data ['global_params'] ['g_adr_house_sch']
-            adr_house_sch_l = yaml_data ['global_params'] ['g_adr_house_sch_l']
-            schemas = (adr_area_sch, adr_street_sch, adr_house_sch, adr_house_sch_l)
-            
-        else:
-            fserver_nmb = None
-            schemas = None
-            
-        rc = 0
-        
-        for host in yaml_data ['hosts']:
-            h_name     = host ['name']
-            h_descr    = host ['descr']
-            h_conninfo = host ['conninfo'] 
-            h_params   = host ['params'] 
-            #
-            id_region       = h_params ['id_region']
-            batch_file_name = h_params ['exec']
-            path            = h_params ['path']
-            # 
-            batch_file_name = path + PATH_DELIMITER + batch_file_name
-            
-            cinfos = string.split (h_conninfo, SPACE_0)
-            for cinfo in cinfos:
-                
-                z = string.split (cinfo, S_DELIM)
-                if z[0] == 'host':
-                    host_ip = z[1]
-                elif z[0] == 'port':  
-                    port = z[1]
-                elif z[0] == 'dbname':
-                    db_name = z[1]
-                elif z[0] == 'user':
-                    user_name = z[1]
-                else:
-                    password = z[1]
-            
-            version_date = sys.argv[2]
-            # 
-            print POINTS + h_descr + ", id_region: " + str(id_region) + ", Execute: "\
-                + batch_file_name
-            
-            ml = LoadGar.make_load (host_ip, port, db_name, user_name)
-            rc = ml.to_do (host_ip, port, db_name, user_name, batch_file_name,\
-                bOUT_NAME, bERR_NAME, path, version_date, fserver_nmb, schemas, id_region)     
-            
-            if not (rc == 0):
-                sys.exit (rc)
-            
         sys.exit (rc)
 
 #---------------------------------------

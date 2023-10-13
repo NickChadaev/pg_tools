98e129b6872389d772e545cda963ef1928e8a4d7
Чадаев Николай <n.chadaev.tech>
2023-10-13 12:55:54 +0300

    Пересбока процессингового пакета.

--- fias_loader/DBMS/GAR_TMP_PCG_TRANS/gar_tmp_trans_pcg.sql
+++ fias_loader/DBMS/GAR_TMP_PCG_TRANS/gar_tmp_trans_pcg.sql
@@ -5,7 +5,7 @@
 --
 CREATE OR REPLACE VIEW gar_tmp_pcg_trans.version
  AS
- SELECT '$Revision:657e939$ modified $RevDate:2023-10-11$'::text AS version; 
+ SELECT '$Revision:1555bdb$ modified $RevDate:2023-10-13$'::text AS version; 
                                                            
 
 -- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -3987,40 +3987,33 @@ CREATE OR REPLACE PROCEDURE gar_tmp_pcg_trans.p_gar_fias_crt_idx (p_sw boolean =
    $$
     -- -----------------------------------------------------------------------
     --    2021-11-25/2021-12-09/2022-04-12/2022-07-12/2022-09-05
-    --     Nick. Управление индексами в схеме "gar_fias"
+    --        Nick. Управление индексами в схеме "gar_fias"
+    --   2023-10-13 Изменён первый блок. Далее для "as_addr_obj", 
+    --                "as_adm_hierarchy", "as_houses".
     -- -----------------------------------------------------------------------
     --     p_sw boolean = TRUE  - удаление и создание индексов
     --                    FALSE - только удаление
     -- -----------------------------------------------------------------------
      BEGIN
+       -- 
+       -- 2023-10-13
+       
        DROP INDEX IF EXISTS ie1_as_object_level;
-       DROP INDEX IF EXISTS ie1_as_addr_obj;
-       DROP INDEX IF EXISTS ie1_as_adm_hierarchy; 
+       DROP INDEX IF EXISTS ie1_as_addr_obj;       -- см. 2022-07-12, 2022-09-05
+       DROP INDEX IF EXISTS ie1_as_adm_hierarchy;  -- см. 2022-09-05
        DROP INDEX IF EXISTS ie1_as_mun_hierarchy;
-       DROP INDEX IF EXISTS ie1_as_houses;
+       DROP INDEX IF EXISTS ie1_as_houses;         -- см. 2022-04-12
        --
        IF (p_sw) THEN
+      --
+            CREATE INDEX ie1_as_object_level  ON gar_fias.as_object_level (end_date);
+            CREATE INDEX ie1_as_addr_obj      ON gar_fias.as_addr_obj (end_date);
+            CREATE INDEX ie1_as_adm_hierarchy ON gar_fias.as_adm_hierarchy (parent_obj_id);
+            CREATE INDEX ie1_as_mun_hierarchy ON gar_fias.as_mun_hierarchy (parent_obj_id); 
+            CREATE INDEX ie1_as_houses        ON gar_fias.as_houses (end_date); 
        
-           CREATE INDEX ie1_as_object_level 
-             ON gar_fias.as_object_level (end_date, start_date)  -- level_id, 2022-04-12 
-                                                          WHERE (is_active);
-           --  
-           CREATE INDEX ie1_as_addr_obj 
-             ON gar_fias.as_addr_obj (obj_level, end_date, start_date) 
-                                                          WHERE (is_actual AND is_active);
-           --                                               
-           CREATE INDEX ie1_as_adm_hierarchy 
-                ON gar_fias.as_adm_hierarchy (parent_obj_id, end_date, start_date) 
-                                                          WHERE (is_active); 
-           --
-           CREATE INDEX ie1_as_mun_hierarchy 
-                ON gar_fias.as_mun_hierarchy (parent_obj_id, end_date, start_date) 
-                                                          WHERE (is_active);
-           --  2021-12-09                                               
-                  
-           CREATE INDEX ie1_as_houses   
-                ON gar_fias.as_houses (end_date, start_date) WHERE (is_actual AND is_active);                                    
        END IF;
+       --
        --
        DROP INDEX IF EXISTS ie1_as_addr_obj_params;   
        DROP INDEX IF EXISTS ie1_as_apartments_params; 
@@ -4050,7 +4043,7 @@ CREATE OR REPLACE PROCEDURE gar_tmp_pcg_trans.p_gar_fias_crt_idx (p_sw boolean =
                         ON gar_fias.as_steads_params (type_id, start_date, end_date);
        END IF;	
        --
-       --  2022-04-12
+       --  2022-04-12/2023-10-13
        --
        DROP INDEX IF EXISTS gar_fias.iex_as_houses; 
        DROP INDEX IF EXISTS gar_fias.iex_as_steads; 
@@ -4061,18 +4054,11 @@ CREATE OR REPLACE PROCEDURE gar_tmp_pcg_trans.p_gar_fias_crt_idx (p_sw boolean =
       
        IF (p_sw) THEN
        
-         CREATE INDEX iex_as_houses
-          ON gar_fias.as_houses USING hash (object_guid)
-          WHERE is_actual AND is_active;   
-         --       
-         CREATE INDEX iex_as_steads
-              ON gar_fias.as_steads USING hash (object_guid)
-          WHERE is_actual AND is_active;       
-         --
-         CREATE INDEX ie1_as_steads
-             ON gar_fias.as_steads USING btree
-                 (end_date ASC NULLS LAST, start_date ASC NULLS LAST)
-             WHERE is_actual AND is_active;         
+         -- 2023-10-13
+         CREATE INDEX iex_as_houses ON gar_fias.as_houses USING hash (object_guid);
+         CREATE INDEX iex_as_steads ON gar_fias.as_steads USING hash (object_guid);
+         CREATE INDEX ie1_as_steads ON gar_fias.as_steads USING btree (end_date);
+         -- 2023-10-13
          --
          CREATE INDEX ie1_as_operation_type
              ON gar_fias.as_operation_type USING btree
@@ -4089,24 +4075,24 @@ CREATE OR REPLACE PROCEDURE gar_tmp_pcg_trans.p_gar_fias_crt_idx (p_sw boolean =
                  (object_id ASC NULLS LAST) WHERE is_active;
        END IF;
        --
-       -- 2022-07-12
+       -- 2022-07-12/2023-10-13
        --
        DROP INDEX IF EXISTS ie2_as_addr_obj;
        --
        IF (p_sw) THEN
        
          CREATE INDEX IF NOT EXISTS ie2_as_addr_obj 
-             ON gar_fias.as_addr_obj  USING btree (upper(object_name))  
-                   WHERE (is_actual AND is_active); 
+             ON gar_fias.as_addr_obj  USING btree (upper(object_name));  
+                  -- WHERE (is_actual AND is_active); 
                    
        END IF;
        --
-       -- 2022-09-05
+       -- 2022-09-05/2023-10-13
        --
        DROP INDEX IF EXISTS ie1_gap_adr_area;
        DROP INDEX IF EXISTS ie2_gap_adr_area;
        DROP INDEX IF EXISTS ie3_as_addr_obj ;
-       DROP INDEX IF EXISTS ie4_as_addr_obj ;
+       DROP INDEX IF EXISTS ie4_as_addr_obj ; -- Дубль  2023-10-13
        DROP INDEX IF EXISTS ie2_as_adm_hierarchy; 
        
        IF (p_sw) THEN
@@ -4117,15 +4103,20 @@ CREATE OR REPLACE PROCEDURE gar_tmp_pcg_trans.p_gar_fias_crt_idx (p_sw boolean =
            CREATE INDEX IF NOT EXISTS ie2_gap_adr_area ON gar_fias.gap_adr_area 
                    USING btree (obj_level);
            --
-           CREATE INDEX IF NOT EXISTS ie3_as_addr_obj ON gar_fias.as_addr_obj  
+           CREATE INDEX IF NOT EXISTS ie3_as_addr_obj ON gar_fias.as_addr_obj    -- ???
                    USING btree (object_guid, end_date, start_date) WHERE (is_actual AND is_active); 
            -- 
-           CREATE INDEX IF NOT EXISTS ie4_as_addr_obj ON gar_fias.as_addr_obj 
-                   USING btree (end_date, start_date) WHERE (is_actual AND is_active);
+           -- CREATE INDEX IF NOT EXISTS ie4_as_addr_obj ON gar_fias.as_addr_obj  --Дубль
+           --         USING btree (end_date, start_date) WHERE (is_actual AND is_active);
            -- 
+           -- CREATE INDEX IF NOT EXISTS ie2_as_adm_hierarchy ON gar_fias.as_adm_hierarchy 
+           --         USING btree (end_date, start_date) WHERE (is_active); -- Потенциальная ошибка в
+           --         -- функциях -- отображения ?? Сейчас только одно условие:  
+           --        -- признак активности IS TRUE 
+           -- --   
            CREATE INDEX IF NOT EXISTS ie2_as_adm_hierarchy ON gar_fias.as_adm_hierarchy 
-                   USING btree (end_date, start_date) WHERE (is_active);
-                       
+                                     USING btree (end_date); -- Потенциальная ошибка в
+                  
        END IF;
        
      END;

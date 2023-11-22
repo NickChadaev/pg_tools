d04d457b9d9f91fbb8b5488ff316e469533363e3
Чадаев Николай <n.chadaev.tech>
2023-11-22 13:10:07 +0300

    Сборка процессингового пакета

--- fias_loader/DBMS/GAR_TMP_PCG_TRANS/version.sql
+++ fias_loader/DBMS/GAR_TMP_PCG_TRANS/version.sql
@@ -3,5 +3,5 @@
 --
 CREATE OR REPLACE VIEW gar_tmp_pcg_trans.version
  AS
- SELECT '$Revision:704332c$ modified $RevDate:2023-11-16$'::text AS version; 
+ SELECT '$Revision:fe31532$ modified $RevDate:2023-11-22$'::text AS version; 
                                                            

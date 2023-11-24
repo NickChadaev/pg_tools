2c26418606d6e414f9bfa31aff817d1818d26115
Чадаев Николай <n.chadaev.tech>
2023-11-24 17:40:36 +0300

    DB_EXCHANGE - сборка функционального пакета.  20+10

--- db_exchange/UIO/PROCEDURE/p_event_ins.sql
+++ db_exchange/UIO/PROCEDURE/p_event_ins.sql
@@ -16,6 +16,7 @@ AS
  --  2023-03-06 Макет, живёт пока не выяснятся USE CASE  pgq.
  --  2023-04-28  Далее.
  --  2023-05-15  Topic 10+10
+ --  2023-11-23  Topic 20+10 
  -- ===========================================================
   DECLARE
   
@@ -35,7 +36,20 @@ AS
     cP7 CONSTANT text = 'uio.event_p7';
     cP8 CONSTANT text = 'uio.event_p8';
     cP9 CONSTANT text = 'uio.event_p9';
-    
+    --
+    -- Next 2023-11-23
+    --
+    cP10 CONSTANT text = 'uio.event_p10';
+    cP11 CONSTANT text = 'uio.event_p11';
+    cP12 CONSTANT text = 'uio.event_p12';
+    cP13 CONSTANT text = 'uio.event_p13';
+    cP14 CONSTANT text = 'uio.event_p14';
+    cP15 CONSTANT text = 'uio.event_p15';
+    cP16 CONSTANT text = 'uio.event_p16';
+    cP17 CONSTANT text = 'uio.event_p17';
+    cP18 CONSTANT text = 'uio.event_p18';
+    cP19 CONSTANT text = 'uio.event_p19';
+    --
     cR0 CONSTANT text = 'uio.event_r0';
     cR1 CONSTANT text = 'uio.event_r1';
     cR2 CONSTANT text = 'uio.event_r2';
@@ -90,7 +104,20 @@ AS
       WHEN 'QP7' THEN _exec = format(_insert, cP7, p_ev_type, p_ev_data, p_ev_extra1, p_ev_extra2, p_ev_extra3, p_ev_extra4);            
       WHEN 'QP8' THEN _exec = format(_insert, cP8, p_ev_type, p_ev_data, p_ev_extra1, p_ev_extra2, p_ev_extra3, p_ev_extra4);            
       WHEN 'QP9' THEN _exec = format(_insert, cP9, p_ev_type, p_ev_data, p_ev_extra1, p_ev_extra2, p_ev_extra3, p_ev_extra4);            
-      
+      --
+      -- 2023-11-23
+      --
+      WHEN 'QP10' THEN _exec = format(_insert, cP10, p_ev_type, p_ev_data, p_ev_extra1, p_ev_extra2, p_ev_extra3, p_ev_extra4);            
+      WHEN 'QP11' THEN _exec = format(_insert, cP11, p_ev_type, p_ev_data, p_ev_extra1, p_ev_extra2, p_ev_extra3, p_ev_extra4);            
+      WHEN 'QP12' THEN _exec = format(_insert, cP12, p_ev_type, p_ev_data, p_ev_extra1, p_ev_extra2, p_ev_extra3, p_ev_extra4);            
+      WHEN 'QP13' THEN _exec = format(_insert, cP13, p_ev_type, p_ev_data, p_ev_extra1, p_ev_extra2, p_ev_extra3, p_ev_extra4);            
+      WHEN 'QP14' THEN _exec = format(_insert, cP14, p_ev_type, p_ev_data, p_ev_extra1, p_ev_extra2, p_ev_extra3, p_ev_extra4);            
+      WHEN 'QP15' THEN _exec = format(_insert, cP15, p_ev_type, p_ev_data, p_ev_extra1, p_ev_extra2, p_ev_extra3, p_ev_extra4);            
+      WHEN 'QP16' THEN _exec = format(_insert, cP16, p_ev_type, p_ev_data, p_ev_extra1, p_ev_extra2, p_ev_extra3, p_ev_extra4);            
+      WHEN 'QP17' THEN _exec = format(_insert, cP17, p_ev_type, p_ev_data, p_ev_extra1, p_ev_extra2, p_ev_extra3, p_ev_extra4);            
+      WHEN 'QP18' THEN _exec = format(_insert, cP18, p_ev_type, p_ev_data, p_ev_extra1, p_ev_extra2, p_ev_extra3, p_ev_extra4);            
+      WHEN 'QP19' THEN _exec = format(_insert, cP19, p_ev_type, p_ev_data, p_ev_extra1, p_ev_extra2, p_ev_extra3, p_ev_extra4);            
+      --
       WHEN 'QR0' THEN _exec = format(_insert, cR0, p_ev_type, p_ev_data, p_ev_extra1, p_ev_extra2, p_ev_extra3, p_ev_extra4);            
       WHEN 'QR1' THEN _exec = format(_insert, cR1, p_ev_type, p_ev_data, p_ev_extra1, p_ev_extra2, p_ev_extra3, p_ev_extra4);            
       WHEN 'QR2' THEN _exec = format(_insert, cR2, p_ev_type, p_ev_data, p_ev_extra1, p_ev_extra2, p_ev_extra3, p_ev_extra4);            
@@ -110,7 +137,7 @@ AS
  $$;   
 --
 COMMENT ON PROCEDURE uio.p_event_ins (text, text, text, text, text, text, text) 
-   IS  'МАКЕТ. Создать события в очередях';
+   IS  'Создать события в очередях';
 -- USE CASE                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
 --     CALL uio.p_event_ins ('QP', 'TP', 'DP', 'DP1', 'DP2', 'DP3', 'DP4');
 --     CALL uio.p_event_ins ('QR', 'TR', 'DR', 'DR1', 'DR2', 'DR3', 'DR4');

665a2c3adcb22ae17b78e11dfa4a24a0f9d0402b
Чадаев Николай <n.chadaev.tech>
2024-01-20 19:26:40 +0300

    Заплатка.

--- fias_loader/DBMS/GAR_TMP_PCG_TRANS/DO/adr_area_post_proc_90.sql
+++ fias_loader/DBMS/GAR_TMP_PCG_TRANS/DO/adr_area_post_proc_90.sql
@@ -8,6 +8,20 @@ BEGIN;
      
    SELECT * FROM gar_tmp.xxx_adr_area_gap; 
    SELECT * FROM gar_fias.twin_addr_objects WHERE (obj_level IN(0,1,2)) ORDER BY obj_level DESC; 
+   --
+   -- 2024-01-20
+   
+   -- Необходимо проверить работу обновления эталона, куда грузятся обновления из ФИАС.
+   -- Пример ошибки:
+   -- ошибочный индекс у села в Дагестане: Дагестан Респ, Новолакский р-н, Чапаево с
+   -- Индекс ФИАС: 368160
+   -- Индекс в нашем адресном справочнике: 368168   
+   
+   SELECT * FROM gar_tmp.adr_area 
+                             WHERE (nm_fias_guid = '1a91684d-7af5-4f5e-acf3-d33fe8e3351a');
+   UPDATE gar_tmp.adr_area SET nm_zipcode = '368160'  
+                             WHERE (nm_fias_guid = '1a91684d-7af5-4f5e-acf3-d33fe8e3351a');   
+---
 
 --ROLLBACK;
 COMMIT;

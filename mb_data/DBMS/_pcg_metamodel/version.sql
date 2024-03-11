f09bd11ee741cbf7072ede64434e3f96ac99b091
Чадаев Николай <n.chadaev.tech>
2024-03-11 10:08:08 +0300

    Первая сборка pcg_dict

--- mb_data/DBMS/_pcg_metamodel/version.sql
+++ mb_data/DBMS/_pcg_metamodel/version.sql
@@ -1,7 +1,7 @@
 --
 -- Версия пакета. Дата общей сборки, либо максимальная дата обновления.
 --
-CREATE OR REPLACE VIEW pcg_dict.version
+CREATE OR REPLACE VIEW pcg_metamodel.version
  AS
  SELECT '$Revision:4b501d3$ modified $RevDate:2024-03-07$'::text AS version; 
                                                            

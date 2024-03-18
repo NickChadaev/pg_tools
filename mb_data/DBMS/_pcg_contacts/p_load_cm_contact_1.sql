a4aa691d2b7e8aa2b754eae27eab065f7a417819
Чадаев Николай <n.chadaev.tech>
2024-03-18 20:46:29 +0300

    Равёртывание metabse.  Загрузка TRIAL-1  контакты (по организациям: 22)

--- mb_data/DBMS/_pcg_contacts/p_load_cm_contact_1.sql
+++ mb_data/DBMS/_pcg_contacts/p_load_cm_contact_1.sql
@@ -1,46 +1,80 @@
---Отдельно добавляем атрибуты сообщений ЛК, чтобы загрзука отрабатывала быстрее
-with upd_attr_lk
-  as (SELECT id_contact,
-             coalesce(jsonb_build_object('KD_SOFTWARE',
-                                         case
-                                           when nm_device like 'mozilla/5.0%%' then 1 --Браузер
-                                           when nm_device is null then 3 --Не найдено
-                                           else 2 --В остальных случаях это приложение
-                                         end,
-                                         'KD_DEVICE',
-                                         case
-                                           when nm_device like '%windows nt%' or nm_device like '%macintosh%' then 2 --ПК
-                                           when nm_device like '%android%'
-                                             or nm_device like '%iphone%'
-                                             or nm_device like '%ipad%'
-                                             or (nm_device is not null and nm_device not like 'mozilla/5.0%%' )then 1 --Телефон
-                                           else 3 --Не найдено
-                                         end), '{}'::jsonb) vl_data
-        FROM dblink ('ccrm',
-                     format($$select cc.id_contact,
-                                     split_part(substring(cm.nm_message from 'Приложение:</b> (.*)'), '<br', 1) nm_app,
-                                     lower(split_part(substring(cm.nm_message from 'Устройство:</b> (.*)'), '<br', 1)) nm_device,
-                                     split_part(substring(cm.nm_message from 'Версия ОС:</b> (.*)'), '<br', 1) nm_ver_os
-                                from contacts.cm_contact cc
-                              join contacts.cm_contact_service ccs --Берем только первое обращение процесса
-                                on cc.id_contact = ccs.id_contact
-                              join contacts.cm_message_service cms --Берем только первое сообщение по обращению
-                                on ccs.id_service = cms.id_service
-                               and cms.id_message = (select min(id_message)
-                                                       from contacts.cm_message_service
-                                                     where id_service = cms.id_service)
-                              join contacts.cm_message cm
-                                on cms.id_message = cm.id_message
-                               and cm.kd_tp_contact = 1
-                              where cc.kd_system = 104
-                                and cc.id_contact_method = 3
-                                and cm.dt_message >= %1L
-                                and cm.dt_message < %2L$$, p_dt_start, p_dt_end)) 
-        AS cm_contact (id_contact bigint,
-                       nm_app text,
-                       nm_device text,
-                       nm_ver_os text))
-update contacts.cm_contact cc
-   set data = coalesce(data, '{}'::jsonb) || ual.vl_data
-from upd_attr_lk ual
-where cc.id_contact = ual.id_contact;
+DROP PROCEDURE IF EXISTS pcg_contacts.p_load_cm_contact_1 (timestamp, timestamp,  bigint);
+CREATE OR REPLACE PROCEDURE pcg_contacts.p_load_cm_contact_1 (
+   p_dt_start    timestamp
+  ,p_dt_end      timestamp
+  ,p_id_facility bigint   
+)  
+  LANGUAGE plpgsql
+  SECURITY INVOKER
+AS
+$$
+ DECLARE
+   _exec  text = format ($_$ 
+            SELECT cc.id_contact,
+                       split_part (substring(cm.nm_message from 'Приложение:</b> (.*)'), '<br', 1) nm_app,
+                       lower(split_part (substring(cm.nm_message from 'Устройство:</b> (.*)'), '<br', 1)) nm_device,
+                       split_part (substring(cm.nm_message from 'Версия ОС:</b> (.*)'), '<br', 1) nm_ver_os
+                  FROM contacts.cm_contact cc
+                
+                JOIN contacts.cm_contact_service ccs --Берем только первое обращение процесса
+                  ON (cc.id_contact = ccs.id_contact)
+                
+                JOIN contacts.cm_message_service cms --Берем только первое сообщение по обращению
+                  ON ccs.id_service = cms.id_service
+                 AND cms.id_message = (SELECT min(id_message)
+                                         FROM contacts.cm_message_service
+                                          WHERE id_service = cms.id_service
+                                       )
+                                       
+                JOIN contacts.cm_message cm ON cms.id_message = cm.id_message
+                                              AND cm.kd_tp_contact = 1
+                INNER JOIN dict.acc_user u ON (cm.id_usr = u.acc_id_usr)                              
+                WHERE cc.kd_system = 104 AND cc.id_contact_method = 3 AND 
+                      cm.dt_message >= %1$L AND cm.dt_message < %2$L AND 
+                      u.id_facility = %3$L
+             $_$, p_dt_start, p_dt_end, p_id_facility
+    );
+ 
+ BEGIN
+   -- Отдельно добавляем атрибуты сообщений ЛК, чтобы загрузка отрабатывала быстрее
+   
+   WITH upd_attr_lk
+     AS (SELECT id_contact,
+                COALESCE (jsonb_build_object('KD_SOFTWARE',
+                             CASE
+                               WHEN nm_device LIKE 'mozilla/5.0%%' THEN 1 --Браузер
+                               WHEN nm_device IS NULL THEN 3 --Не найдено
+                               ELSE 2 --В остальных случаях это приложение
+                             END,
+                             
+                             'KD_DEVICE',
+                             CASE
+                               WHEN nm_device LIKE '%windows nt%' OR nm_device LIKE '%macintosh%' THEN 2 --ПК
+                               WHEN nm_device LIKE '%android%'
+                                 OR nm_device LIKE '%iphone%'
+                                 OR nm_device LIKE '%ipad%'
+                                 OR (nm_device IS NOT NULL AND nm_device NOT LIKE 'mozilla/5.0%%') THEN 1 --Телефон
+                               ELSE 3 --Не найдено
+                             END), '{}'::jsonb
+                         ) vl_data
+           FROM dblink ('ccrm', _exec) 
+           AS cm_contact ( id_contact bigint 
+                          ,nm_app     text 
+                          ,nm_device  text 
+                          ,nm_ver_os  text
+                        )
+   )
+   UPDATE contacts.cm_contact cc
+      SET DATA = COALESCE (data, '{}'::jsonb) || ual.vl_data
+   FROM upd_attr_lk ual
+   WHERE (cc.id_contact = ual.id_contact);
+
+ END;
+$$;
+ 
+COMMENT ON PROCEDURE pcg_contacts.p_load_cm_contact_1 (timestamp, timestamp, bigint)
+     IS 'Отдельно добавляем атрибуты сообщений ЛК';
+     --
+     -- USE CASE:
+     --
+     -- CALL pcg_contacts.p_load_cm_contact_1 ('2023-01-14', '2024-03-14', 22);     
--- mb_data/DBMS/_pcg_contacts/p_load_cm_contact_status_hist.sql
+++ mb_data/DBMS/_pcg_contacts/p_load_cm_contact_status_hist.sql
@@ -1,20 +1,53 @@
+DROP PROCEDURE IF EXISTS pcg_contacts.p_load_cm_contact_status_hist (timestamp, timestamp, bigint);
 
-   
-insert into contacts.cm_contact_status_hist (id_contact, dt_change, kd_status, id_usr)
-SELECT * 
-  FROM dblink ('ccrm',
-               format($$select sh.id_sys_entity id_contact, 
-                               sh.dt_change, 
-       					       sh.kd_status, 
-       					       sh.id_usr 
-  				          from contacts.cm_status_hist sh
-				        where sh.kd_sys_entity = 5
-  				          and sh.dt_change >= %1L
-                          and sh.dt_change < %2L$$, p_dt_start, p_dt_end)) 
-  AS cm_contact_status_hist (id_contact bigint, 
-                             dt_change timestamp, 
-       					     kd_status int4, 
-       					     id_usr integer)
-where exists (select from contacts.cm_contact
-              where id_contact = cm_contact_status_hist.id_contact)                 
-on conflict (id_contact, dt_change) do nothing;
+CREATE OR REPLACE PROCEDURE pcg_contacts.p_load_cm_contact_status_hist ( 
+                    p_dt_start    timestamp 
+                   ,p_dt_end      timestamp
+                   ,p_id_facility bigint   
+)
+  LANGUAGE plpgsql
+  SECURITY INVOKER
+ AS
+$$
+ DECLARE
+   _exec text = format ($_$ SELECT  sh.id_sys_entity id_contact
+                                     ,sh.dt_change 
+          					         ,sh.kd_status 
+          					         ,sh.id_usr 
+     				          FROM contacts.cm_status_hist sh
+     				            INNER JOIN dict.acc_user u ON (sh.id_usr = u.acc_id_usr ) 
+     				          
+   				        WHERE sh.kd_sys_entity = 5 AND sh.dt_change >= %1$L
+                             AND sh.dt_change < %2$L AND u.id_facility = %3$L
+                        $_$, p_dt_start, p_dt_end, p_id_facility
+    );
+ 
+ BEGIN  
+   INSERT INTO contacts.cm_contact_status_hist (
+         id_contact
+       , dt_change
+       , kd_status
+       , id_usr
+       )
+   SELECT * 
+     FROM dblink ('ccrm', _exec ) 
+     AS cm_contact_status_hist ( id_contact bigint, 
+                                 dt_change timestamp, 
+          					     kd_status int4, 
+          					     id_usr  integer
+          					     )
+   WHERE EXISTS (SELECT FROM contacts.cm_contact
+                 WHERE id_contact = cm_contact_status_hist.id_contact
+                 )                 
+   ON CONFLICT (id_contact, dt_change) DO NOTHING;
+
+ END;
+$$;
+ 
+COMMENT ON PROCEDURE pcg_contacts.p_load_cm_contact_status_hist (timestamp, timestamp, bigint)
+     IS 'Загрузка истории статуса обращения';
+     --
+     -- USE CASE:
+     --
+     -- CALL pcg_contacts.p_load_cm_contact_status_hist ('2023-01-14', '2024-03-14', 22)
+     -- SELECT * FROM contacts.cm_contact_status_hist;
